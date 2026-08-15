442919874c6ff40d2d55759c49727e0c921c302e
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 15:05:44 +0530

    add department information on dashboard

--- dashboard.cpp
+++ dashboard.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    cout<<"Welcome to your dashboard";
+    cout<<"******* Dashboard *******"<<endl;
+    cout<<"Welcome to your dashboard"<<endl;
+    cout<<"Student Information"<<endl;
+
+    cout<<"Name : Nirupama Ranjan"<<endl;
+    cout<<"Roll no: 1234"<<endl;
+    cout<<"Department: CSE"<<endl;
+
+    
+
 }
