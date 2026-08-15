385c0c7a1c3f16af2d88c592f28541e253fb416c
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 14:48:53 +0530

    add student details in profile page

--- profile.cpp
+++ profile.cpp
@@ -1,6 +1,11 @@
-#include<stdio.h>
+#include<iostream>
 using namespace std;
 
 int main(){
     cout<<"****** Profile *******"<<endl;
+    cout<<"Name: Nirupama Ranjan"<<endl;
+    cout<<"Roll no: 1234"<<endl;
+    cout<<"Degree : M.Tech"<<endl;
+    
+
 }
