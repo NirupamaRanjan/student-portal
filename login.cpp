b7923e081bb7dc4ea33771a39fa6a936293abd09
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 14:24:19 +0530

    add credentials validation logic

--- login.cpp
+++ login.cpp
@@ -6,5 +6,16 @@ using namespace std;
 int main(){
     string username,password;
     cout<<"***** Login Page *****";
-    
+
+    cout<<"Enter Username:"<<endl;
+    cin>>username;
+
+    cout<<"Enter Password:"<<endl;
+    cin>>password;
+
+    if(username=="nirupama" && password=="123")
+    cout<<"Login successful!";
+    else cout<<"Invalid credentials!";
+
+
 }
