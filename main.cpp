f5eebb4e43c08acb6586933eb78ef5dfbe8973ca
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 18:12:12 +0530

    update version to v1.0

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main(){
    
     int nav;
-    cout<<"Portal v1.0"<<endl;
-    cout << "****** Student Portal *****" << endl;
+    cout << "****** Student Portal v1.0 *****" << endl;
     cout<<"Welcome to the student portal!!"<<endl;
     cout << "1. Login" << endl;
     cout << "2. View Dashboard" << endl;
