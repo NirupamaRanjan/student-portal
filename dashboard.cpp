8353ef7b752e24dac565a797e223ad63795c080e
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 17:35:08 +0530

    complete new feature after stash recovery

--- dashboard.cpp
+++ dashboard.cpp
@@ -14,10 +14,17 @@ int main(){
     cout<<"Department: CSE"<<endl;
 
     cout<<"Extra curriculum activities";
-    for(int i=0;i<4;i++){
+    for(int i=0;i<v.size();i++){
         cout<<v[i]<<endl;
     }
 
     cout<<"This section displays achievments of your college"<<endl;
+     
+    //work in progress
+    vector<string>chosen={"Cricket","Hockey"};
+     cout<<"These are activities you have selected"<<endl;
+    for(int i=0;i<chosen.size();i++){
+        cout<<chosen[i]<<endl;
+    }
 
 }
