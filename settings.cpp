a6b812c4321f980b4af8c23165ec781e34cf7c91
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 16:36:47 +0530

    add notification settings

--- settings.cpp
+++ settings.cpp
@@ -5,10 +5,16 @@ int main(){
     bool dark_mode=false;
 
     cout<<"******* Settings *******"<<endl;
-    cout<<"Click here to switch to dark mode"<<endl;
-    dark_mode=(~dark_mode);
+    cout<<"1.Click here to switch to dark mode"<<endl;
+    dark_mode=(!dark_mode);
 
-    cout<<"Click here to chage profile picture";
+    cout<<"2.Click here to chage profile picture";
+
+    bool notification=false;
+    cout<<"3.Click here to turn on notifications";
+    notification=!notification;
+
     
+
 
 }
