4c14b2150f8ce9398b352f622ebfce5a7cc7b80e
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 15:45:06 +0530

    rectify grade calculation logic

--- grades.cpp
+++ grades.cpp
@@ -5,4 +5,14 @@ int main(){
     cout<<"Welcome to grades page"<<endl;
     string subjects[] = {"Math", "Physics", "CS", "English"};
     float  grades[]   = {3.7, 3.3, 4.0, 3.0};
+
+    cout << "*** My Grades ***" << endl;
+    int n = 4;
+    float total  = 0;
+
+    for (int i = 0; i < n; i++) {
+        cout << subjects[i] << " : " << grades[i] << endl;
+        total += grades[i];
+    }
+
 }
