d48d4294accc9258a2abace8adc6198f231d10b9
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2026-08-15 16:18:55 +0530

    add dark mode setting

--- settings.cpp
+++ settings.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
+    bool dark_mode=false;
+
+    cout<<"******* Settings *******"<<endl;
+    cout<<"Click here to switch to dark mode"<<endl;
+    dark_mode=(~dark_mode);
+
     
+
 }
